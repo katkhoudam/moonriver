09dcda36c35497790701154f24b54ecde14d07c9
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2025-06-25 09:47:40 +0300

    Update page.js

--- app/menu/page.js
+++ app/menu/page.js
@@ -5,7 +5,7 @@ export default function Menu() {
          <div class="h-screen flex-col p-4 justify-items-center">
        <Image src="/menu1.png" alt="" width="800" height="1000" /> 
 <Image src="/menu2.png" alt="" width="800" height="1000" /> 
-<Image src="/menu4.png" alt="" width="800" height="1000" /> 
+<Image src="/menu4.png" alt="" width="800" height="800" /> 
 <Image src="/chefrec.png" width={700} height={1000} alt="" />
 <Footer />
             
